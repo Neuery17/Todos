5a216b23d121048390400ae3a550a9c70263352e
Neuery17Alt <jannickangerer@gmail.com>
2023-08-04 21:04:32 +0200

    added contentBoxes.vue, added div-boxes

---
+++ src/components/contentBoxes.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="cards">
+      <h2>
+        Make your life easier
+      </h2>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </p>
+    </div>
+  <div class="cards">
+    <h2>
+      Make managing life easy
+    </h2>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+      et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+      ipsum dolor sit amet.
+    </p>
+  </div>
+  <div class="cards">
+    <h2>
+      Save time by just a few clicks
+    </h2>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+      invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+      et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+      ipsum dolor sit amet.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.cards {
+  box-shadow: 0 0 8px 5px #1bb683;
+  width: 15%;
+  height: auto;
+  text-align: center;
+  float: left;
+  border: black 1px solid;
+  border-radius: 10px;
+  margin-right: 20px;
+  margin-top: 3%;
+  padding: 5px
+}
+
+</style>

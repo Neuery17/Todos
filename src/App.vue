e19a146bf5ffec8467555bb537fb955e1aae696e
Neuery17Alt <jannickangerer@gmail.com>
2023-08-04 22:18:08 +0200

    updated App.vue, added Footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script setup>
+import "./assets/main.css"
+import Header from "@/components/Header.vue";
 
 </script>
 
 <template>
+  <Header></Header>
   <router-view></router-view>
+
+  <footer id="copyrightFooter">
+    <hr id="seperatorCopyright">
+    <label id="copyright">Copyright © 2023 by Jannick Angerer </label>
+  </footer>
+
 </template>
 
 
 <style scoped>
+#copyright {
+  color: lightgrey;
+  position: fixed;
+  width: 100%;
+}
+
+#seperatorCopyright {
+  border-top: 1px solid lightgrey;
+}
+
+#copyrightFooter {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 5%;
+}
+
 
 
 </style>

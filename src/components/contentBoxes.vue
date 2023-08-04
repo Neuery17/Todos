13053f85ba5ef4c780f2d70caa4916c64df21e9d
Neuery17Alt <jannickangerer@gmail.com>
2023-08-04 22:18:27 +0200

    updated contentBoxes.vue

--- src/components/contentBoxes.vue
+++ src/components/contentBoxes.vue
@@ -25,7 +25,7 @@
   </div>
   <div class="cards">
     <h2>
-      Save time by just a few clicks
+      Save time
     </h2>
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
       invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam

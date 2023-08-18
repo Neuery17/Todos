5a658bc2aeb8b86857f14df88e1e240cf51ca5b5
Neuery17Alt <jannickangerer@gmail.com>
2023-08-19 01:25:54 +0200

    updated SignUpView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
 <script setup>
 import "./assets/main.css"
 import Header from "@/components/Header.vue";
-
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <Header></Header>
   <hr id="seperatorHeader">
   <router-view></router-view>
-
-  <footer id="copyrightFooter">
-    <hr id="seperatorCopyright">
-    <label id="copyright">Copyright © 2023 by Jannick Angerer </label>
-  </footer>
-
+  <Footer></Footer>
 </template>
 
-
 <style scoped>
-#copyright {
-  color: lightgrey;
-}
-
-#seperatorCopyright {
-  border-top: 1px solid lightgrey;
-}
-
-#copyrightFooter {
-  margin-top: 5%;
-  margin-bottom: 0;
-}
-
 #seperatorHeader {
   border-top: 1px solid lightgrey;
   width: 100%;
 }
-
 </style>

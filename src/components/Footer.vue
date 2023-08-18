4d92fa5a560d786da721d4668e1a06e115aa9a07
Neuery17Alt <jannickangerer@gmail.com>
2023-08-19 01:27:50 +0200

    updated SignUpView.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <footer id="copyrightFooter">
+    <hr id="seperatorCopyright">
+    <label id="copyright">Copyright © 2023 by Jannick Angerer </label>
+  </footer>
+</template>
+
+<style scoped>
+#copyright {
+  color: lightgrey;
+}
+
+#seperatorCopyright {
+  border-top: 1px solid lightgrey;
+}
+
+#copyrightFooter {
+  margin-top: 5%;
+  margin-bottom: 0;
+}
+
+footer {
+  bottom: 0;
+}
+</style>
